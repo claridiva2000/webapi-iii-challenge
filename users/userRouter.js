3e9ffcb85877c0a00ba87098bcd550e6f433b31f
claridiva2000 <claridiva2000@gmail.com>
2019-05-17 20:04:53 -0500

    mvp done.

--- users/userRouter.js
+++ users/userRouter.js
@@ -82,7 +82,7 @@ router.delete('/:id', async (req, res) => {
     });
   }
 });
-
+ 
 router.put('/:id', async (req, res) => {
   try {
     const user = await Users.update(req.params.id, req.body);
